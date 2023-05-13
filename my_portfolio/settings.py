87f29db81a25b29494e7e25014b4106c2f33256d
Mateusz Urban <58361072+cyrisek@users.noreply.github.com>
2023-05-13 12:21:56 +0000

    debug false

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -39,7 +39,7 @@ PGPORT = os.environ.get('PGPORT')
 PGUSER = os.environ.get('PGUSER')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
